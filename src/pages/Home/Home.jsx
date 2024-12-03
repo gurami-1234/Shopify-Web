700251ccaead5798e1415118aaed3425f146c7cd
gurami-1234 <gukadavitadze22@gmail.com>
2024-12-03 19:49:42 +0400

    Products page is done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,12 +4,15 @@ import ProductsLayout from "layouts/ProductsLayout/ProductsLayout";
 import React, { useEffect, useState } from "react";
 const Home = () => {
   const [trendingProducts, setTrendingProducts] = useState([]);
+  const [newArrivalProducts, setNewArrivalProducts] = useState([]);
+  let skipN = 10;
   let limit = 8;
   let skip = 0;
   useEffect(() => {
     getProducts(limit, skip).then((resp) => setTrendingProducts(resp));
+    getProducts(limit, skipN).then((resp) => setNewArrivalProducts(resp));
   }, []);
-
+  useEffect(() => {}, []);
   return (
     <div>
       <ProductsLayout title={"Trending Products"}>
@@ -25,8 +28,22 @@ const Home = () => {
           />
         ))}
       </ProductsLayout>
-
-      <ProductsLayout title={"New Feature"} />
+      <hr />
+      <hr />
+      <hr />
+      <hr />
+      <ProductsLayout title={"New Feature"}>
+        {newArrivalProducts.map((el) => (
+          <CardItem
+            key={el.id}
+            title={el.title}
+            category={el.category}
+            oldPrice={el.price}
+            discountPercentage={el.discountPercentage}
+            thumbnail={el.thumbnail}
+          />
+        ))}
+      </ProductsLayout>
     </div>
   );
 };
--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,19 +1,55 @@
+import { CardItem } from "components";
 import { getProducts } from "helper/api";
+import ProductsLayout from "layouts/ProductsLayout/ProductsLayout";
 import React, { useEffect, useState } from "react";
-
+import "./Products.css";
 const Products = () => {
   const [productList, setProductList] = useState([]);
+  const [skip, setSkip] = useState(0);
+  const [limit, setLimit] = useState(28);
 
   useEffect(() => {
-    getProducts(30, 0).then((resp) => setProductList(resp));
-  }, []);
+    getProducts(limit, skip).then((resp) => setProductList(resp));
+  }, [skip]);
+
+  const changePage = (e) => {
+    let page = (e.target.value - 1) * limit;
+    setSkip(page);
+    window.scroll({
+      top: 0,
+      behavior: "smooth",
+    });
+    console.log(window);
+  };
   return (
     <div>
-      <ol>
-        {productList.map((el) => {
-          return <li key={el.id}>{el.title}</li>;
-        })}
-      </ol>
+      <ProductsLayout title="Products">
+        {productList.map((el) => (
+          <CardItem
+            key={el.id}
+            title={el.title}
+            category={el.category}
+            oldPrice={el.price}
+            discountPercentage={el.discountPercentage}
+            thumbnail={el.thumbnail}
+            id={el.id}
+          />
+        ))}
+      </ProductsLayout>
+      <div className="page-btns">
+        <button className="page-btn" value={1} onClick={changePage}>
+          1
+        </button>
+        <button className="page-btn" value={2} onClick={changePage}>
+          2
+        </button>
+        <button className="page-btn" value={3} onClick={changePage}>
+          3
+        </button>
+        <button className="page-btn" value={4} onClick={changePage}>
+          4
+        </button>
+      </div>
     </div>
   );
 };
